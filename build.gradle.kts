f08d1568d9df109aa083a3192483ba335b78c517
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-01-19 12:43:23 +0100

    gitignore, project settings part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+//    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
 }
 
 group = "com.dehydrogenaza"
 version = "1.0-SNAPSHOT"
+
+val kotlinVersion = KotlinVersion.KOTLIN_1_8
+val jvmVersion = 17
+
 
 repositories {
     mavenCentral()
@@ -20,5 +26,18 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+//    kotlinOptions.jvmTarget = "1.8"
+//    kotlinOptions {
+//        apiVersion = "1.8"
+//        languageVersion = "1.8"
+//        jvmTarget = "17"
+//    }
+
+    compilerOptions.apiVersion.set(kotlinVersion)
+    compilerOptions.languageVersion.set(kotlinVersion)
+    //compilerOptions.jvmTarget.set(JvmTarget.JVM_17) //set automatically by the toolchain
+}
+
+kotlin {
+    jvmToolchain(jvmVersion)
 }
