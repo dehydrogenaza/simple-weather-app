ba810dc7ddf8d6b0eb031425341047a02a547ce3
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-01-27 18:10:28 +0100

    0.14.2 MenuAction refactor, using Regexes now and allowing aliases

--- src/main/kotlin/controller/Menu.kt
+++ src/main/kotlin/controller/Menu.kt
@@ -4,7 +4,7 @@ import controller.actions.MenuAction
 import ui.UI
 
 class Menu(private val prompt: String?, private val defaultAction: MenuAction, vararg actions: MenuAction) {
-    private val nonDefaultCommands: Map<String, MenuAction> = actions.associateBy { it.command }
+    private val nonDefaultCommands: Map<MenuAction, Regex?> = actions.associateWith { it.command }
     fun loop() {
         do {
             val input = UI.ask(prompt).trim().lowercase()
@@ -12,6 +12,10 @@ class Menu(private val prompt: String?, private val defaultAction: MenuAction, v
     }
 
     private fun loopIteration(input: String): Boolean {
-        return nonDefaultCommands[input]?.perform() ?: defaultAction.perform()
+        nonDefaultCommands.forEach { (action, command) ->
+            command?.let { if (input.matches(it)) return action.perform() }
+        }
+        return defaultAction.perform()
+        //return nonDefaultCommands[input]?.perform() ?: defaultAction.perform()
     }
 }
