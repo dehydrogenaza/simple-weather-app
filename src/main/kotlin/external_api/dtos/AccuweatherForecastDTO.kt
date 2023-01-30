4daafbd0879347909d25e8cad7ab6a3950916b5a
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-01-30 15:39:00 +0100

    0.18.1 weather DTO

---
+++ src/main/kotlin/external_api/dtos/AccuweatherForecastDTO.kt
@@ -0,0 +1,91 @@
+package external_api.dtos
+
+import com.google.gson.annotations.SerializedName
+
+class AccuweatherForecastDTO : ApiDTO {
+    @SerializedName("Headline")
+    lateinit var headline: HeadlineDto
+
+    @SerializedName("DailyForecasts")
+    lateinit var dailyForecasts: Array<DailyForecastDto>
+
+    inner class HeadlineDto(
+        @SerializedName("Text")
+        val description: String,
+
+        @SerializedName("EffectiveDate")
+        val startDate: String,
+
+        @SerializedName("EndDate")
+        val endDate: String
+    )
+
+    inner class DailyForecastDto {
+        @SerializedName("Temperature")
+        lateinit var temp: TemperatureDto
+
+        @SerializedName("RealFeelTemperature")
+        lateinit var feltTemp: TemperatureDto
+
+        @SerializedName("Day")
+        lateinit var day: DayNightDto
+
+        @SerializedName("Night")
+        lateinit var night: DayNightDto
+
+        inner class TemperatureDto {
+            @SerializedName("Minimum")
+            lateinit var min: TemperatureValueDto
+
+            @SerializedName("Maximum")
+            lateinit var max: TemperatureValueDto
+
+            inner class TemperatureValueDto(
+                @SerializedName("Value")
+                val celsius: Double
+            )
+        }
+
+        inner class DayNightDto(
+            @SerializedName("PrecipitationChance")
+            val precipitationChance: Int
+        ) {
+            @SerializedName("LongPhrase")
+            lateinit var description: String
+
+            @SerializedName("PrecipitationType")
+            lateinit var precipitationType: String
+
+            @SerializedName("Wind")
+            lateinit var wind: WindDto
+
+            @SerializedName("WindGust")
+            lateinit var windGust: WindDto
+
+            @SerializedName("TotalLiquid")
+            lateinit var liquidVolume: LiquidDto
+
+            inner class WindDto {
+                @SerializedName("Speed")
+                lateinit var speed: WindSpeedDto
+
+                @SerializedName("Direction")
+                lateinit var direction: WindDirectionDto
+                inner class WindSpeedDto(
+                    @SerializedName("Value")
+                    val kmh: Int
+                )
+
+                inner class WindDirectionDto(
+                    @SerializedName("English")
+                    val dir: String
+                )
+            }
+
+            inner class LiquidDto(
+                @SerializedName("Value")
+                val mm: Double
+            )
+        }
+    }
+}
